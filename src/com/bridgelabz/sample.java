e6317c68acfc9a62b2e513ec4aa58af7bd036a23
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-17 20:27:21 +0530

    ADDED: Ability to serch person in a city or state across the multiply address book

---
+++ src/com/bridgelabz/sample.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.util.function.Predicate;
+
+public class sample {
+    public static void main(String[] args) {
+        Predicate<Integer> predicate ;
+    }
+}
