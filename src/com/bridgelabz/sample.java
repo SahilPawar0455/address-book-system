265a6bcc285c814916c549a0631d22c7d5a96f28
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-19 19:55:50 +0530

    ADDED: Ability to serch the person by city or state

--- src/com/bridgelabz/sample.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz;
-
-import java.util.function.Predicate;
-
-public class sample {
-    public static void main(String[] args) {
-        Predicate<Integer> predicate ;
-    }
-}
