8d4fcd6c793bbedbb04fd76f26e3642954963dc3
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-22 20:05:18 +0530

    ADDED: Ability to sort by city or state or zip code

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -15,6 +15,7 @@ public class AddressBookMain {
         System.out.println(addressBookMap);
         countCityOfPerson();
         sortByPersonName();
+        sortByCityOrState();
     }
 
     private static void multiplyAddressBook() {
@@ -42,17 +43,19 @@ public class AddressBookMain {
             addressBook.searchContact();
         }
     }
+
     private static void countCityOfPerson() {
         System.out.println("Enter the name of Addresses book");
         String cityOrstate = scanner.next();
         AddressBook addressBook = new AddressBook();
         AddressBook address = addressBookMap.get(cityOrstate);
-        if (address == null){
+        if (address == null) {
             System.out.println("No book is found in address book");
-        }else {
+        } else {
             addressBook.countCityOrPerson();
         }
     }
+
     public static void sortByPersonName() {
         System.out.println("Enter the name of Addresses book");
         String cityOrstate = scanner.next();
@@ -62,6 +65,32 @@ public class AddressBookMain {
             System.out.println("No book is found in address book");
         } else {
             addressBook.sortByPersonName();
+        }
+    }
+
+    private static void sortByCityOrState() {
+        System.out.println("Enter the name of Addresses book");
+        String name = scanner.next();
+        AddressBook addressBook = new AddressBook();
+        AddressBook address = addressBookMap.get(name);
+        if (address == null) {
+            System.out.println("No book is found in address book");
+        } else {
+            System.out.println("Choose the number \n1.sort by city \n2.sort by state \n3.sort by zip code");
+            int number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    addressBook.sortByCity();
+                    break;
+                case 2:
+                    addressBook.sortByState();
+                    break;
+                case 3:
+                    addressBook.sortByZipCode();
+                    break;
+                default:
+                    System.out.println("enter the wrong number");
+            }
         }
     }
 }
